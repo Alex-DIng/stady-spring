4f8152e6d2c05b606ab85b16c7ba92923dcd4b5f
root <ppote@163.com>
2020-06-19 15:30:42 +0800

    thymeleaf 引用片段

--- src/main/java/tk/dingjining/studyspring/controller/LoginController.java
+++ src/main/java/tk/dingjining/studyspring/controller/LoginController.java
@@ -5,10 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import tk.dingjining.studyspring.entity.User;
+
 @Controller
 public class LoginController {
 	@GetMapping("/login")
 	public String toLogin(HttpServletRequest request) {
+		request.setAttribute("user", new User(1, "lisi", "123"));
 		request.setAttribute("name", "zhangsan");
 		return "login";
 	}
